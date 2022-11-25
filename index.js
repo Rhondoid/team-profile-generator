430699a916dee7882cf55c8df1ec33b6830b41c2
Rhonda <110504360+Rhondoid@users.noreply.github.com>
2022-11-25 16:43:19 -0700

    added jest, employee files

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+
+
+
+// Array of questions to setup employees
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    email: "What is the employee email?",
+    getName()
+  },
+  
+  function init() {
+
+  }
---
+++ tests/employee.test.js
@@ -0,0 +1,6 @@
+
+test('1 plus 1 should be equal to 2', (=> {
+    let a = 1;
+    let b =1;
+expect(a + b).toBe(1)
+}))
